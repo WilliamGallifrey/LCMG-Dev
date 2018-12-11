e54076dd8e96211d63d3a0ec9470c0a1bfbcde08
WilliamGallifrey <guigarro23@gmail.com>
2018-12-11 13:17:54 +0100

    reboot

---
+++ js/code.js
@@ -0,0 +1,71 @@
+
+var patt = new RegExp('-');
+
+
+$(document).ready(function(){
+
+	urlStr = window.location.href;
+    urlStr = urlStr.substring(urlStr.indexOf('#') + 1, urlStr.length);
+    switch (urlStr) {
+        case "inicio":
+            init();
+            break;
+        case 'http://localhost/LCMG/':
+            init();
+            break;
+        default:
+            if (patt.test(urlStr)) {
+                var urlId = urlStr.substring(0, urlStr.indexOf('-'));
+                var urlColo = urlStr.substring(urlStr.indexOf('-') + 1, urlStr.length);
+                prod(urlId, urlColo)
+            } else {
+                //404()
+            }
+    }
+
+    window.onpopstate = function(event) {
+        if (event.state) {
+            $('#content-main').css('opacity', '0');
+            state = event.state;
+            shareid = state.shid;
+            sharecolor = state.shcolor;
+            setTimeout(function() {
+                $('head title', window.parent.document).text(state.tab);
+                $("body").css('background-color', state.background);
+                $('#content-main').html(state.content);
+                $('#content-main').css('opacity', '1')
+            }, 730)
+        }
+    };
+
+});
+
+function init() {
+
+    $('head title', window.parent.document).text('LCMG | Inicio');
+    $("meta[name='theme-color']").attr('content', '#FFF');
+    $("meta[name='msapplication-navbutton-color']").attr('content', '#FFF');
+    $("meta[name='apple-mobile-web-app-capable']").attr('content', 'yes');
+    $("meta[name='apple-mobile-web-app-status-bar-style']").attr('content', 'black-translucent');
+    $('#content-main').css('opacity', '0');
+    $(window).scrollTop(0);
+    setTimeout(function() {
+        $.ajax({
+            type: "GET",
+            url: "php/init.php",
+            success: function(data) {
+                
+                $('#content-main').html(data);
+                state = {
+                    content: data,
+                    background: "#FFF",
+                    tab: "LCMG | Inicio"
+                };
+                window.history.pushState(state, null, "#inicio");
+                $('#content-main').css('opacity', '1');
+            }
+        });
+        $("body").css('background-color', '#1b1b1b');
+        $('#content-main').css("background-color", "rgba(255,255,255,0.1)")
+    }, 730)
+}
---
+++ php/init.php
@@ -0,0 +1,206 @@
+<?php 
+
+echo"
+
+
+
+<span class=\"sectionTitle\">Más Populares</span>
+  <div class=\"row\">
+    
+
+    
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted1\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+
+    </div>
+    
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted2\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted3\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+    <div class=\"col-lg-3 col-md-12 col-sm-6\">
+      
+      <div class=\"box higlighted4\">
+        
+
+        <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+
+  </div>
+
+  <span class=\"sectionTitle\">Destacados</span>
+  <div class=\"row\">
+    
+    
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted1\">        
+
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+
+    </div>
+    
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted2\">        
+
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+    <div class=\"col-lg-3 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted3\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+    <div class=\"col-lg-3 col-md-12 col-sm-6\">
+      
+      <div class=\" box higlighted4\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+
+  </div>
+
+
+  <span class=\"sectionTitle\">Entrevistas</span>
+
+  <div class=\"row\">
+    
+    
+    <div class=\"col-lg-4 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted1\">
+        
+        <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div> 
+
+        </div>
+
+      </div>
+
+    
+    
+    <div class=\"col-lg-4 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted2\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+    <div class=\"col-lg-4 col-md-4 col-sm-6\">
+      
+      <div class=\"box higlighted3\">
+        
+          <div class=\"modalbox\">
+            
+              <h5>Titulo</h5>
+              <p>subtítulo</p>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+    
+
+
+    </div>
+  
+
+    </div>
+
+
+
+
+";
+
+ ?>
